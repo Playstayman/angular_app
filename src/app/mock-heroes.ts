abdb54054b2a9ecaec26a406c1c21f52d53f9533
Playstayman <avolzhev@gmail.com>
2021-07-16 10:55:42 +0200

    module 4 comp

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+
+    { id: 11, name: '1' },
+    { id: 12, name: '2' },
+    { id: 13, name: '3' },
+    { id: 14, name: '4' },
+    { id: 15, name: '5' },
+    { id: 16, name: '6' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Magma' },
+    { id: 19, name: 'Tornado' },
+    { id: 20, name: '7' }
+];
